def9f8f7e544b0b8068a3ec54f3135460fe842ec
Natalia Puzina <puzinanata@gmail.com>
2025-05-20 16:37:24 +0100

    WIP: step 2. installation gzip

---
+++ steps/2_cross_tmp_tools/10_gzip.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# for debug on remote comp directly
+#PACKAGE_CACHE=/var/lib/lfs
+#LFS=/mnt/new_root_dir
+#BUILD_DIR=/mnt/new_root_dir/build
+#LFS_TGT=$(uname -m)-lfs-linux-gnu
+#JOBS=$(nproc)
+
+PACKAGE_URL='https://ftp.gnu.org/gnu/gzip/gzip-1.13.tar.xz'
+PACKAGE_NAME='gzip-1.13.tar.xz'
+PACKAGE_MD5='d5c9fc9441288817a4a0be2da0249e29'
+PACKAGE_DIR_NAME='gzip-1.13'
+
+pushd "${PACKAGE_CACHE}"
+
+if [ ! -f "${PACKAGE_NAME}" ]; then
+  curl -LO "${PACKAGE_URL}"
+fi
+
+cp -v "${PACKAGE_NAME}" "${BUILD_DIR}/"
+pushd "${BUILD_DIR}"
+
+MD5_ACTUAL=$(md5sum "${PACKAGE_NAME}"| awk '{ print $1 }')
+
+if [[ "${MD5_ACTUAL}" == "${PACKAGE_MD5}"  ]]; then
+    tar -xJf "${PACKAGE_NAME}"
+    echo "unpacked successfully."
+
+    rm "${PACKAGE_NAME}"
+    echo "Archive removed."
+
+else
+    echo "MD5 mismatch!"
+fi
+
+#Go to unpacked dir with source
+pushd "${PACKAGE_DIR_NAME}"
+
+#Configure
+./configure --prefix=/usr   \
+            --host=$LFS_TGT
+
+#Build
+make -j$JOBS
+
+#Install build artefacts from previous step
+make DESTDIR=$LFS install
+
+popd
+popd
+popd
