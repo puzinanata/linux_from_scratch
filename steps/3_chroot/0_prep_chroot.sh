953f96624b0903799932c2e7e900139c30091c7c
Natalia Puzina <puzinanata@gmail.com>
2025-06-06 11:37:32 +0300

    WIP: auto download packages for chroot

--- steps/3_chroot/0_prep_chroot.sh
+++ steps/3_chroot/0_prep_chroot.sh
@@ -9,12 +9,8 @@ mkdir -pv "$PACKAGE_CACHE"
 download_package() {
   local PACKAGE_URL="$1"
   local PACKAGE_NAME="$2"
-  local PACKAGE_NAME="$3"
-  local PACKAGE_NAME="$4"
-  local PACKAGE_NAME="$5"
-  local PACKAGE_NAME="$6"
 
-  pushd "$PACKAGE_CACHE"
+  pushd "$PACKAGE_CACHE" > /dev/null
 
   if [ ! -f "$PACKAGE_NAME" ]; then
     echo ">>> Downloading $PACKAGE_NAME ..."
@@ -23,7 +19,7 @@ download_package() {
     echo ">>> $PACKAGE_NAME already exists. Skipping download."
   fi
 
-  popd
+  popd > /dev/null
 }
 
 # Package 1: gettext
@@ -43,3 +39,34 @@ download_package "https://ftp.gnu.org/gnu/texinfo/texinfo-7.2.tar.xz" "texinfo-7
 
 # Package 6: util-linux
 download_package "https://www.kernel.org/pub/linux/utils/util-linux/v2.40/util-linux-2.40.4.tar.xz" "util-linux-2.40.4.tar.xz"
+
+# Package 7: man-pages
+download_package "https://www.kernel.org/pub/linux/docs/man-pages/man-pages-6.12.tar.xz" "man-pages-6.12.tar.xz"
+
+# Package 8: Iana-etc
+download_package "https://github.com/Mic92/iana-etc/releases/download/20250123/iana-etc-20250123.tar.gz" "iana-etc-20250123.tar.gz"
+
+# Package 9: glibc
+download_package "https://ftp.gnu.org/gnu/glibc/glibc-2.41.tar.xz" "glibc-2.41.tar.xz"
+
+# Package 10: zlib
+download_package "https://zlib.net/fossils/zlib-1.3.1.tar.gz" "zlib-1.3.1.tar.gz"
+
+# Package 11: bzip
+download_package "https://www.sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz" "bzip2-1.0.8.tar.gz"
+
+# Package 12: xz
+download_package "https://github.com//tukaani-project/xz/releases/download/v5.6.4/xz-5.6.4.tar.xz" "xz-5.6.4.tar.xz"
+
+# Package 13: lz4
+download_package "https://github.com/lz4/lz4/releases/download/v1.10.0/lz4-1.10.0.tar.gz" "lz4-1.10.0.tar.gz"
+
+# Package 14: zstd
+download_package "https://github.com/facebook/zstd/releases/download/v1.5.7/zstd-1.5.7.tar.gz" "zstd-1.5.7.tar.gz"
+
+# Package 15: file
+download_package "https://astron.com/pub/file/file-5.46.tar.gz" "file-5.46.tar.gz"
+
+# Package 16: readline
+download_package "https://ftp.gnu.org/gnu/readline/readline-8.2.13.tar.gz" "readline-8.2.13.tar.gz"
+
