59ea1f4998b081435dc590ab9c075ceca673a0d9
Natalia Puzina <puzinanata@gmail.com>
2025-07-12 12:58:59 +0100

    WIP: configuration LFS

--- steps/3_chroot/91_network_config.sh
+++ steps/3_chroot/91_network_config.sh
@@ -14,23 +14,19 @@ EOF
 popd
 
 cat > /etc/resolv.conf << "EOF"
-# Begin /etc/resolv.conf
 
 nameserver 8.8.8.8
 
-
-# End /etc/resolv.conf
 EOF
 
 echo "lfs" > /etc/hostname
 
+
 cat > /etc/hosts << "EOF"
-# Begin /etc/hosts
 
 127.0.0.1 localhost.localdomain localhost
 ::1       localhost ip6-localhost ip6-loopback
 ff02::1   ip6-allnodes
 ff02::2   ip6-allrouters
 
-# End /etc/hosts
 EOF
--- steps/3_chroot/92_config.sh
+++ steps/3_chroot/92_config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cat > /etc/inittab << "EOF"
-# Begin /etc/inittab
 
 id:3:initdefault:
 
@@ -27,27 +26,87 @@ s1:1:respawn:/sbin/sulogin
 5:2345:respawn:/sbin/agetty tty5 9600
 6:2345:respawn:/sbin/agetty tty6 9600
 
-# End /etc/inittab
 EOF
 
 cat > /etc/sysconfig/clock << "EOF"
-# Begin /etc/sysconfig/clock
 
 UTC=1
 
-# Set this to any options you might need to give to hwclock,
-# such as machine hardware clock type for Alphas.
 CLOCKPARAMS=
 
-# End /etc/sysconfig/clock
 EOF
 
 cat > /etc/sysconfig/console << "EOF"
-# Begin /etc/sysconfig/console
 
 UNICODE="1"
 FONT="Lat2-Terminus16"
 
-# End /etc/sysconfig/console
+EOF
+
+cat > /etc/profile << "EOF"
+
+for i in $(locale); do
+  unset ${i%=*}
+done
+
+if [[ "$TERM" = linux ]]; then
+  export LANG=C.UTF-8
+else
+  export LANG=en_US.UTF-8
+fi
+
+EOF
+
+cat > /etc/inputrc << "EOF"
+# Begin /etc/inputrc
+# Modified by Chris Lynn <roryo@roryo.dynup.net>
+
+# Allow the command prompt to wrap to the next line
+set horizontal-scroll-mode Off
+
+# Enable 8-bit input
+set meta-flag On
+set input-meta On
+
+# Turns off 8th bit stripping
+set convert-meta Off
+
+# Keep the 8th bit for display
+set output-meta On
+
+# none, visible or audible
+set bell-style none
+
+# All of the following map the escape sequence of the value
+# contained in the 1st argument to the readline specific functions
+"\eOd": backward-word
+"\eOc": forward-word
+
+# for linux console
+"\e[1~": beginning-of-line
+"\e[4~": end-of-line
+"\e[5~": beginning-of-history
+"\e[6~": end-of-history
+"\e[3~": delete-char
+"\e[2~": quoted-insert
+
+# for xterm
+"\eOH": beginning-of-line
+"\eOF": end-of-line
+
+# for Konsole
+"\e[H": beginning-of-line
+"\e[F": end-of-line
+
+# End /etc/inputrc
+EOF
+
+cat > /etc/shells << "EOF"
+# Begin /etc/shells
+
+/bin/sh
+/bin/bash
+
+# End /etc/shells
 EOF
 
